c3f48e848e3d7d695efcac4c8aad16d3553ffc8d
Fabio Sansone <sansone@srvauger01.na.infn.it>
2025-03-26 11:18:42 +0100

    Solved a small bug

--- TestSFP.py
+++ TestSFP.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 from SFP import SFP
 from I2CSwitch import I2CSwitch
+from functools import partial
 
 SWITCH_ADDR = 0x70
 SFP_ADDR = 0x50
 
 
 switch = I2CSwitch(i2c_bus=2, i2c_addr=SWITCH_ADDR)
-sfp = SFP(i2c_bus=2, i2c_addr=SFP_ADDR, i2c_select=(lambda:switch.select(0),))
+sfp = SFP(i2c_bus=2, i2c_addr=SFP_ADDR, i2c_select=(partial(switch.select, 0),))
 
 
 print(switch.get_channel_debug())
