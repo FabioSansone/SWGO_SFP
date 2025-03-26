69b5cd944ebd3824d741fb3586de39b390bda9a0
Fabio Sansone <sansone@srvauger01.na.infn.it>
2025-03-26 10:34:16 +0100

    Solved a small bug

--- SFP.py
+++ SFP.py
@@ -32,7 +32,8 @@ class SFP:
     def vendor(self):
         self.select()
         vendor = self.bus.read_i2c_block_data(self.i2c_addr, self.REG_VENDOR, 16)
-        return str.rstrip(''.join(map(chr, vendor)))
+        return bytes(vendor).decode('ascii', errors='ignore').strip()
+        #return str.rstrip(''.join(map(chr, vendor)))
 
     def model(self):
         self.select()
