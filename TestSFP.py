e0cbb8fc137e99ebb923720ad37e38a84c04bec4
Fabio Sansone <sansone@srvauger01.na.infn.it>
2025-03-26 10:42:51 +0100

    Solved a small bug

--- TestSFP.py
+++ TestSFP.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 from SFP import SFP
 from I2CSwitch import I2CSwitch
 
 switch = I2CSwitch(i2c_bus=2, i2c_addr=0x70)
-sfp = SFP(i2c_bus=2, i2c_addr=0x70, i2c_select=(lambda:switch.select(0),))
+sfp = SFP(i2c_bus=2, i2c_addr=0x70, i2c_select=(lambda:switch.select(1),))
 
 print("SFP #0")
 if sfp.is_available():
