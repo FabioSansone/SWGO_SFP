8fa201ed67b1552cf22cc6e2f1d222b45c35897a
Fabio Sansone <sansone@srvauger01.na.infn.it>
2025-03-26 10:02:58 +0100

    Added SFP, I2CSwitch, and TestSFP for I2C communication with SFP modules via PCA9546A switch

---
+++ SFP.py
@@ -0,0 +1,91 @@
+from smbus2 import SMBus
+
+class SFP:
+
+    REG_VENDOR = 0x14
+    REG_MODEL = 0x28
+    REG_SERIAL = 0x44
+    REG_DATECODE = 0x54
+    REG_TEMPERATURE = 0x60
+    REG_VCC = 0x62
+    REG_TXBIAS = 0x64
+    REG_TXPOWER = 0x66
+    REG_RXPOWER = 0x68
+
+    def __init__(self, i2c_bus, i2c_addr, i2c_select=()):
+        self.i2c_addr = i2c_addr
+        self.i2c_select = i2c_select
+        self.bus = SMBus(i2c_bus)
+
+    def select(self):
+        for func in self.i2c_select:
+            func()
+
+    def is_available(self):
+        self.select()
+        try:
+            self.bus.read_byte_data(self.i2c_addr, 0x00)
+        except:
+            return False
+        return True
+
+    def vendor(self):
+        self.select()
+        vendor = self.bus.read_i2c_block_data(self.i2c_addr, self.REG_VENDOR, 16)
+        return str.rstrip(''.join(map(chr, vendor)))
+
+    def model(self):
+        self.select()
+        model = self.bus.read_i2c_block_data(self.i2c_addr, self.REG_MODEL, 20)
+        return str.rstrip(''.join(map(chr, model)))
+
+    def serial(self):
+        self.select()
+        serial = self.bus.read_i2c_block_data(self.i2c_addr, self.REG_SERIAL, 16)
+        return str.rstrip(''.join(map(chr, serial)))
+
+    def datecode(self):
+        self.select()
+        datecode = self.bus.read_i2c_block_data(self.i2c_addr, self.REG_DATECODE, 8)
+        return str.rstrip(''.join(map(chr, datecode)))
+
+    def temperature(self):
+        self.select()
+        tlist = self.bus.read_i2c_block_data(self.i2c_addr+1, self.REG_TEMPERATURE, 2)
+        temp = (tlist[0] << 8) + tlist[1]
+        return round(float(temp * 1/256), 2)   # LSB = 1/256 C
+      
+    def voltage(self):
+        self.select()
+        vlist = self.bus.read_i2c_block_data(self.i2c_addr+1, self.REG_VCC, 2)
+        voltage = (vlist[0] << 8) + vlist[1]
+        return round(float(voltage * 1E-4), 2)  # LSB = 100 uV
+
+    def tx_bias(self):
+        self.select()
+        blist = self.bus.read_i2c_block_data(self.i2c_addr+1, self.REG_TXBIAS, 2)
+        bias = (blist[0] << 8) + blist[1]
+        return round(float(bias * 2)/1000.0, 2)  # LSB = 2 uA
+
+    def tx_power(self):
+        self.select()
+        plist = self.bus.read_i2c_block_data(self.i2c_addr+1, self.REG_TXPOWER, 2)
+        power = (plist[0] << 8) + plist[1]
+        return round(float(power * 0.1), 2)  # LSB = 0.1 uW
+
+    def rx_power(self):
+        self.select()
+        plist = self.bus.read_i2c_block_data(self.i2c_addr+1, self.REG_RXPOWER, 2)
+        power = (plist[0] << 8) + plist[1]
+        return round(float(power * 0.1), 2)  # LSB = 0.1 uW
+
+    def print(self):
+        print(f"vendor: {self.vendor()}")
+        print(f"model: {self.model()}")
+        print(f"serial: {self.serial()}")
+        print(f"datecode: {self.datecode()}")
+        print(f"voltage: {self.voltage()}V")
+        print(f"temperature: {self.temperature()}C")
+        print(f"tx bias: {self.tx_bias()}mA")
+        print(f"tx power: {self.tx_power()}uW")
+        print(f"rx power: {self.rx_power()}uW")
